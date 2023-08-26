9290be91fee41d4bf3f894643373787fd097d7f7
szilviszitar <szilviszitar@gmail.com>
2023-08-26 11:53:00 +0200

    Component collections-elements done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewsModule } from './views/views.module';
+import { ModsModule } from './mods/mods.module';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ViewsModule,
+    ModsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
