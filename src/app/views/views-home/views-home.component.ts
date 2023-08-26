7fe54580ff71ab9a3be5d58db9db987b2912cba7
szilviszitar <szilviszitar@gmail.com>
2023-08-26 20:44:21 +0200

    Views Components

--- src/app/views/views-home/views-home.component.ts
+++ src/app/views/views-home/views-home.component.ts
@@ -4,8 +4,25 @@ import { Component } from '@angular/core';
   selector: 'app-views-home',
   templateUrl: './views-home.component.html',
   styleUrls: ['./views-home.component.css'],
-  
+
 })
 export class ViewsHomeComponent {
+  stats = [
+    { value: 22, label: '# of users' },
+    { value: 900, label: 'Revenue' },
+    { value: 50, label: 'Reviews' }
+  ];
 
+  items:any = [
+    {
+      image: '/assets/images/couch.jpeg',
+      title: 'Couch',
+      description: 'This is a fantastic couch to sit on!'
+    },
+    {
+      image: '/assets/images/dresser.jpeg',
+      title: 'Dresser',
+      description: 'This is a dresser to put stuff in! '
+    }
+  ]
 }
